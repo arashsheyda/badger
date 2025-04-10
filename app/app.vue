81d310873a93b2d57975622f949be8b011fa5895
Arash Ari Sheyda <hi@arashsheyda.me>
2025-04-10 16:30:16 -0600

    fix: adjust layout

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <h1>Badger</h1>
-  </div>
+  <Html>
+    <Body>
+      <NuxtRouteAnnouncer />
+      <main class="min-h-screen bg-gray-100">
+        <div class="container mx-auto p-4">
+          <div class="grid grid-cols-2 gap-8">
+            <div />
+            <div class="flex flex-col gap-4 items-center justify-center">
+              <Badge>
+                <template #canvas>
+                  <canvas
+                    ref="badgeCanvas"
+                    width="444"
+                    height="192"
+                    class="mb-4 border-2"
+                  />
+                </template>
+              </Badge>
+            </div>
+          </div>
+        </div>
+      </main>
+    </Body>
+  </Html>
 </template>
